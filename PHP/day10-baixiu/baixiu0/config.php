02aae5e5896641207e2d85da5b0bd416d8e2962e
javalay <953981413@qq.com>
2018-12-17 23:21:17 +0800

    'p10'

--- PHP/day10-baixiu/baixiu0/config.php
+++ PHP/day10-baixiu/baixiu0/config.php
@@ -4,4 +4,4 @@ define("DB_HOST",'127.0.0.1');	//数据库地址（也可以写localhost)
 define("DB_USER",'root');		//数据库登录名（目前必然是root)
 define("DB_PWD",'');			//数据库密码
 define("DB_NAME",'db_baixiu');	//数据库名
- ?> 
+ ?>   
