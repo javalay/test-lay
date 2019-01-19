3beb6b04d40fabc904f2a4e10a419fca997c48df
kris wu <953981413@qq.com>
2019-01-19 21:17:28 +0800

    'v6.5'

---
+++ Vue/webpack-studyy/src/main.js
@@ -0,0 +1,20 @@
+
+import $ from "jquery"
+import "./css/index.css"
+import "./css/index.less"
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+class Person{
+    static info={name:'zs',age:20}
+}
+function Person1(name){
+    this.name=name
+}
+var p1=new Person1('李拴蛋')
+console.log(Person.info)
+console.log("=========")
+console.log(p1.name)
+
+$(function(){
+    $('li:odd').css("backgroundColor","green")
+    $('li:even').css("backgroundColor","yellow")
+})
